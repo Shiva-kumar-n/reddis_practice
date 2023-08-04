d077650b51543ec24aa52e2ce081771a79964ac7
Shiva Kumar <nagalla.kumar@vedantu.com>
2023-08-04 11:55:20 +0530

    first commit

---
+++ src/main/java/com/example/reddis/ReddisPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.reddis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddisPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddisPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reddis/ReddisPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reddis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddisPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
